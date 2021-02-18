16c71cbba24e6dfb199d03a108722fcb0e71c5b6
Seongbeom Kim <tjdqja1811@naver.com>
2021-02-18 22:28:40 +0900

    Initial commit

---
+++ solve.py
@@ -0,0 +1,6 @@
+import collections
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    result = collections.Counter(participant) - collections.Counter(completion)
+    return list(result)[0]
